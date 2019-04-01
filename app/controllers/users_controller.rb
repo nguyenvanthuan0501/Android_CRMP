024e6885fd41f83a2e138b75c8ad9c8aa253b949
Quy Nguyen <quycao1510@gmail.com>
2019-04-01 16:20:10 +0700

    Add login function for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
    #GET "/users"
   def index
-    @user = User.order(:id)
-
+    @user = User.all
     render json: @user
   end
 
    #POST "/users"
   def create
-    @user = User.new(user_params)
-
+    @user = User.create!(user_params)
     if @user.save
-      # 
       response = {
         user: @user,
         message: "success"
@@ -20,15 +17,28 @@ class UsersController < ApplicationController
       response = {
         message: "error"
       }
-      # render json: @user.errors, status: :unprocessable_entity
     end
-   
+    render json: response
+  end
+
+  def login
+    @user = User.find_by(email: params[:email].downcase)
+    if @user && @user.authenticate(params[:password])
+      response = {
+        user: @user,
+        message: "success"
+      }
+    else
+      response = {
+        message: "login not success"
+      }
+    end
     render json: response
   end
 
   private 
 
   def user_params
-    params.require(:user).permit(:password, :email)
+    params.require(:user).permit(:password, :email, :fullname, :phone_number, :address)
   end
 end
