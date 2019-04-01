62902a775996ddb3f371e0a17252969fe7b68b29
Quy Nguyen <quycao1510@gmail.com>
2019-04-01 16:20:24 +0700

    Add routes for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_040157) do
+ActiveRecord::Schema.define(version: 2019_04_01_081809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_040157) do
     t.integer "id_card_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
