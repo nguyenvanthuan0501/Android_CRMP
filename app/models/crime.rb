dd477b30902c5be2edf983059c4c387365daf24e
QuyNguyen <quycao2010@gmail.com>
2019-04-04 15:57:27 +0700

    Remove validate create_time for crime report

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,6 +1,6 @@
 class Crime < ApplicationRecord
     belongs_to :user
     belongs_to :category
-    validates_presence_of :category_id, :area, :title, :description, :time, :user_id
+    validates_presence_of :category_id, :area, :title, :description, :user_id
     validates_length_of :title, minimum: 6, maximum: 50
 end
