f18c61a14adf79b9ced78a6e035e94a892c0ba01
QuyNguyen <quycao2010@gmail.com>
2019-04-11 16:45:59 +0700

    Add field image to crimes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_075443) do
+ActiveRecord::Schema.define(version: 2019_04_11_093846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_075443) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["category_id"], name: "index_crimes_on_category_id"
     t.index ["user_id"], name: "index_crimes_on_user_id"
   end
